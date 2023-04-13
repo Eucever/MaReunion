7dda13d83396bddb4a68100d0411a16d71a49dba
trist <tristancorvisaart@gmail.com>
2023-04-13 16:25:47 +0200

    Premier commit

--- app/src/main/java/com/example/mareunion/MainActivity.java
+++ app/src/main/java/com/example/mareunion/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.openclassrooms.mareunion.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
