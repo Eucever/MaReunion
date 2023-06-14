2285ba8a05ab630d613dcb6becae775d9efb0a41
trist <tristancorvisaart@gmail.com>
2023-06-14 17:39:28 +0200

    Quatrieme commit

--- app/src/main/java/com/example/mareunion/MainActivity.java
+++ app/src/main/java/com/example/mareunion/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
         final FragmentManager fm = getSupportFragmentManager();
         //traitement de l'action sur le bouton timepicker
